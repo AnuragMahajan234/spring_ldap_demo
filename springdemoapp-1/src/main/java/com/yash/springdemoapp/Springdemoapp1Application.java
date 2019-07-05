45a7ef016a9959894fc5a3fdd8412b953f8557dc
AnuragMahajan234 <anuragmahajan1711@gmail.com>
2019-07-05 16:49:59 +0530

    first Commit
    Signed-off-by: AnuragMahajan234 <anuragmahajan1711@gmail.com>

---
+++ springdemoapp-1/src/main/java/com/yash/springdemoapp/Springdemoapp1Application.java
@@ -0,0 +1,13 @@
+package com.yash.springdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdemoapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springdemoapp1Application.class, args);
+	}
+
+}
